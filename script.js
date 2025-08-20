07c0128ae4f43b2002afa82f9920d498a2847438
vivianvnl <224661527+vivianvnl@users.noreply.github.com>
2025-08-19 19:39:00 -0500

    add hover over squares to add color "trail"

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ const container = document.getElementById("container");
 container.setAttribute("style", 
     "width: 800px; height: 800px; display: flex; flex-wrap: wrap; border: 5px solid black; margin: 30px 50px;");
 
-for (i = 0; i < 256; i++) {
+function addSquares() {
+    for (i = 0; i < 256; i++) {
     const squares = document.createElement("div");
-    squares.setAttribute("style", "width: 50px; height: 50px; box-sizing: border-box; border: 1px solid black; margin: 0; padding: 0;")
+    squares.classList.add("squares");
+    squares.setAttribute("style", 
+    "width: 50px; height: 50px; box-sizing: border-box; background-color: white; border: 1px solid black; margin: 0; padding: 0;")
     container.appendChild(squares)
+    }
 }
+addSquares()
+
+const sqs = document.querySelectorAll(".squares");
+sqs.forEach(sq => {
+    sq.addEventListener('mouseover', () => {
+    sq.style.backgroundColor = "yellow";
+    });
+});
