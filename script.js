863936825054cd25bf3597229b02a016861de74a
vivianvnl <224661527+vivianvnl@users.noreply.github.com>
2025-08-26 12:45:20 -0500

    add grid size readjustment (no button yet)

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const container = document.getElementById("container");
+const gridPx = 800;
+let gridLength = 16;
 container.setAttribute("style", 
-    "width: 800px; height: 800px; display: flex; flex-wrap: wrap; border: 5px solid black; margin: 30px 50px;");
+    "display: flex; flex-wrap: wrap; border: 5px solid black; margin: 30px 50px;");
+container.style.height = gridPx + 'px';
+container.style.width = gridPx +'px';
 
 function addSquares() {
-    for (i = 0; i < 256; i++) {
-    const squares = document.createElement("div");
-    squares.classList.add("squares");
-    squares.setAttribute("style", 
-    "width: 50px; height: 50px; box-sizing: border-box; background-color: white; border: 1px solid black; margin: 0; padding: 0;")
-    container.appendChild(squares)
+    for (i = 0; i < gridLength ** 2; i++) {
+        const squares = document.createElement("div");
+        squares.classList.add("squares");
+        squares.setAttribute("style", 
+        "box-sizing: border-box; background-color: white; border: 1px solid black; margin: 0; padding: 0;")
+        squares.style.height = (gridPx / gridLength) + 'px';
+        squares.style.width = (gridPx / gridLength) +'px';
+        container.appendChild(squares)
     }
 }
 addSquares()
