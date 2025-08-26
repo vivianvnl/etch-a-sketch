1c478fe94ed06b874c153140d832873b4ffaa3fe
vivianvnl <224661527+vivianvnl@users.noreply.github.com>
2025-08-26 14:25:04 -0500

    grid size change with mouseover effect working
    basic project complete!

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const setGridButton = document.getElementById("setGridButton");
 const resetGridButton = document.getElementById("resetGridButton");
 const gridPx = 800;
 let gridLength = 16;
+
+// container settings
 container.setAttribute("style", 
     "display: flex; flex-wrap: wrap; border: 5px solid black; margin: 30px 50px;");
 container.style.height = gridPx + 'px';
@@ -20,7 +22,19 @@ function createGrid() {
     }
 }
 
-// creating the grid
+function changeColor() {
+    const squaresAll = document.querySelectorAll(".squares")
+    squaresAll.forEach(sq => {
+        sq.addEventListener('mouseover', () => {
+        sq.style.backgroundColor = "yellow";
+        });
+    });
+}
+
+createGrid(gridLength);
+changeColor();
+
+// setting the grid
 setGridButton.addEventListener("click", (e) => {
     const removeSquares = document.querySelectorAll(".squares")
     removeSquares.forEach(div => {
@@ -28,14 +42,19 @@ setGridButton.addEventListener("click", (e) => {
     });
 
     gridLength = prompt("What grid length would you like?");
+    if (isNaN(gridLength)) {
+        alert("please only type numbers!!");
+        gridLength = 16;
+    }
+    else if (gridLength > 100) {
+        alert("number too big! less than 100 only please :)");
+        gridLength = 100;
+    }
+    else if (gridLength < 1) {
+        alert("number too small! how about a number greater than 0 :)");
+        gridLength = 1;
+    }
+
     createGrid(gridLength);
+    changeColor();
 });
-
-const squaresAll = document.querySelectorAll(".squares")
-squaresAll.forEach(sq => {
-    sq.addEventListener('mouseover', () => {
-    sq.style.backgroundColor = "yellow";
-    });
-});
-
-createGrid(gridLength);
